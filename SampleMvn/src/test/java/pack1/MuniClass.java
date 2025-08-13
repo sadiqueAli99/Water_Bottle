4ead4369785dfe097dfe62aad2b495980af91a11
Sadique K.M <66101916+sadiqueAli99@users.noreply.github.com>
2025-08-13 16:26:37 +0530

    battle muni started

---
+++ SampleMvn/src/test/java/pack1/MuniClass.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class MuniClass {
+	
+	@Test
+	public void muniDemo()
+	{
+		System.out.println("water battle for muni raj");
+	}
+
+}
